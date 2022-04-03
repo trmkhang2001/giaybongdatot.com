c538fb3fb4bfcb89a60d0c5e3799b3c24d2c61ed
DESKTOP-2SI9LLN\jclov <trmkhang2001@gmail.com>
2022-04-03 12:54:52 +0700

    delete file

--- src/assets/fake-data/hero-slider.js
+++ src/assets/fake-data/hero-slider.js
@@ -1,6 +1,5 @@
 const img1 = require("../images/slider/slide_1.png").default
 const img2 = require("../images/slider/slide_2.png").default
-const img3 = require("../images/slider/slide_3.png").default
 
 const heroSliderData = [
     {
--- src/assets/fake-data/products.js
+++ src/assets/fake-data/products.js
@@ -1,6 +1,5 @@
 const product_01_image_01 = require('../images/products/product-01 (1).jpg').default
 const product_01_image_02 = require('../images/products/product-01 (2).jpg').default
-// const product_01_image_03 = require('../images/products/product-01 (3).jpg').default
 
 const product_02_image_01 = require('../images/products/product-02 (1).jpg').default
 const product_02_image_02 = require('../images/products/product-02 (2).jpg').default
